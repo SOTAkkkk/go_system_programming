7948952451c72a64856e679634cce4187068ca3c
SOTAkkkk <>
2023-07-09 21:52:22 +0900

    transform byte fo string

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
-import "fmt"
-
-// インタフェースを定義
-type Talker interface {
-	Talk()
-}
-
-// 構造体を宣言
-type Greeter struct {
-	name string
-}
-
-// 構造体はTalkerインタフェースで定義されているメソッドを持っている
-func (g Greeter) Talk() {
-	fmt.Printf("Hello, my name is %s\n", g.name)
-}
-
 func main() {
-	// インタフェースの型を持つ変数を宣言
-	var talker Talker
+	// byteArrayは[]byte{0x41, 0x53, 0x43, 0x49, 0x49}
+	byteArray := []byte("ASCII")
+	println("ASCII は ", byteArray)
 
-	// インタフェースを満たす構造体のポインタは代入できる
-	talker = &Greeter{"wozozo"}
-	talker.Talk()
+	// strは"ASCII"
+	str := string([]byte{0x41, 0x53, 0x43, 0x49, 0x49})
+	println("[]byte{0x41, 0x53, 0x43, 0x49, 0x49} は ", str)
+
 }
