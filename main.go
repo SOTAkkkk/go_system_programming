a37c607219f6d71e5c2abb14dc5a44763720cb1c
SOTAkkkk <>
2023-07-09 18:21:10 +0900

    2.2 go interface

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+// インタフェースを定義
+type Talker interface {
+	Talk()
+}
+
+// 構造体を宣言
+type Greeter struct {
+	name string
+}
+
+// 構造体はTalkerインタフェースで定義されているメソッドを持っている
+func (g Greeter) Talk() {
+	fmt.Printf("Hello, my name is %s\n", g.name)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	// インタフェースの型を持つ変数を宣言
+	var talker Talker
+
+	// インタフェースを満たす構造体のポインタは代入できる
+	talker = &Greeter{"wozozo"}
+	talker.Talk()
 }
